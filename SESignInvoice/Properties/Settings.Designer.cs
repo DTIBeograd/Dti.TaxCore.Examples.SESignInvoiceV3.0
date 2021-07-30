354802d3056f273dfd535676703b3e12fafc66ff
Ognjen Pantic <ognjen.pantic@dti.rs>
2021-07-30 12:02:11 +0200

    Last fixes before going public

--- SESignInvoice/Properties/Settings.Designer.cs
+++ SESignInvoice/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Dti.TaxCore.Examples.SESignInvoice.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace Dti.TaxCore.Examples.SESignInvoice.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1234")]
+        [global::System.Configuration.DefaultSettingValueAttribute("2021")]
         public int PIN {
             get {
                 return ((int)(this["PIN"]));
